d5676934e652d4a5c2144771bdb399ae7d34e3dc
alguacil803 <143223663+alguacil803@users.noreply.github.com>
2023-09-23 02:18:13 +0000

    repetition factorial while loop

--- src/examples/d_repetition/main.py
+++ src/examples/d_repetition/main.py
@@ -1,5 +1,7 @@
 import repetition
 
 repetition.display_numbers(2)
-
 repetition.prompt_user()
+repetition.prompt_user()
+repetition.for_intro_loop()
+repetition.for_intro_loop_strings()
--- tests/examples/d_repetition/tests_repetition.py
+++ tests/examples/d_repetition/tests_repetition.py
@@ -9,8 +9,12 @@ class Test_Config(unittest.TestCase):
     def test_configuration(self):
         self.assertEqual(True, test_config())
 
-    def test_sum_of_squares(self):
-        self.assertEqual(sum_of_squares(-1), 0)
-        self.assertEqual(sum_of_squares(3), 14)
-        self.assertEqual(sum_of_squares(4), 30)
-        self.assertEqual(sum_of_squares(5), 55)
+    def test_get_factorial(self):
+        self.assertEqual(get_factorial(4), 24)
+        self.assertEqual(get_factorial(5), 120)
+        self.assertEqual(get_factorial(6), 720)
+    
+    def test_sum_odd_numbers(self):
+        self.assertEqual(sum_odd_numbers(7), 16)
+        self.assertEqual(sum_odd_numbers(9), 25)
+        self.assertEqual(sum_odd_numbers(10), 25)
