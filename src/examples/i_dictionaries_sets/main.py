4f2f4bcddd1cd6d6f2ef4041be18e3d288f20658
alguacil803 <143223663+alguacil803@users.noreply.github.com>
2023-10-23 23:32:27 +0000

    dictionaries

--- src/examples/i_dictionaries_sets/main.py
+++ src/examples/i_dictionaries_sets/main.py
@@ -1,1 +1,16 @@
-#main program
+
+import keyword
+import dictionaries
+
+phonebook = {'Danny' : '312-5698', 'Jose' : '652-6339', 'lola' : '888-8888'}
+#print(phonebook['Danny'])
+#print(phonebook['Jose'])
+#print(phonebook['lola'])
+
+for number in phonebook:
+ print(number, phonebook[number])
+for value in phonebook.values():
+ print  (value)
+
+
+
