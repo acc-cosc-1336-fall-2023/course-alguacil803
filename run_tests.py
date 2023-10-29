e119fdc383a612041dffc9f0276788f10ec34841
alguacil803 <143223663+alguacil803@users.noreply.github.com>
2023-10-29 01:36:09 +0000

    updated homework

--- run_tests.py
+++ run_tests.py
@@ -4,11 +4,11 @@ the file in /tests/homework/b_in_proc_out/tests_in_proc_out
 has the test functions
 '''
 #from tests.examples.i_dictionaries_sets import tests_dictionaries_and_sets
-#from tests.homework.i_dictionaries_sets import tests_dictionaries_and_sets
+from tests.homework.i_dictionaries_sets import tests_dictionaries_and_sets
 #from tests.examples.i_dictionaries_sets import tests_dictionaries_and_sets
 from tests.homework.g_lists_and_tuples import tests_lists_and_tuples
 #from tests.homework.d_repetition import tests_repetition
 
-#suite = unittest.TestLoader().loadTestsFromModule(tests_dictionaries_and_sets)
+suite = unittest.TestLoader().loadTestsFromModule(tests_dictionaries_and_sets)
 suite = unittest.TestLoader().loadTestsFromModule(tests_lists_and_tuples)
 unittest.TextTestRunner(verbosity=2).run(suite)
